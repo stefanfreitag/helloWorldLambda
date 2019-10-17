4e0104f80e9d91ae89bfc86ec5c6e5d3d5d312c3
Stefan Freitag <stefan.freitag@udo.edu>
2019-10-17 09:01:17 +0200

    Upgraded to CDK 1.13.1

--- bin/hello-world-lambda.ts
+++ bin/hello-world-lambda.ts
@@ -4,4 +4,9 @@ import cdk = require('@aws-cdk/core');
 import { HelloWorldLambdaStack } from '../lib/hello-world-lambda-stack';
 
 const app = new cdk.App();
-new HelloWorldLambdaStack(app, 'HelloWorldLambdaStack');
+new HelloWorldLambdaStack(app, 'HelloWorldLambdaStack', {
+    env: {
+        region: 'eu-central-1',
+        account: '544871327925'
+    }
+});
