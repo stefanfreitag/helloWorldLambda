c48d1c67032359817af1dc92a7db4464b275ffd9
Stefan Freitag <stefan.freitag@udo.edu>
2019-10-28 22:01:21 +0100

    Updated .gitignore
    Updated CDK to version 1.15.0

--- bin/hello-world-lambda.ts
+++ bin/hello-world-lambda.ts
@@ -4,9 +4,10 @@ import cdk = require('@aws-cdk/core');
 import { HelloWorldLambdaStack } from '../lib/hello-world-lambda-stack';
 
 const app = new cdk.App();
+const env = {
+    region: app.node.tryGetContext('region') || process.env.CDK_DEFAULT_REGION,
+    account: app.node.tryGetContext('account') || process.env.CDK_DEFAULT_ACCOUNT
+}
 new HelloWorldLambdaStack(app, 'HelloWorldLambdaStack', {
-    env: {
-        region: 'eu-central-1',
-        account: '544871327925'
-    }
+    env: env
 });
