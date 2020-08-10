ebffb82c905b02e4f83421fee0e6544e7dfbf016
Stefan Freitag <stefan.freitag@udo.edu>
2020-08-10 20:56:15 +0200

    fix: fixed broken unit test

--- test/hello-world-lambda.test.ts
+++ test/hello-world-lambda.test.ts
@@ -1,13 +1,17 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import { expect as expectCDK, matchTemplate, MatchStyle, haveResourceLike } from '@aws-cdk/assert';
 import cdk = require('@aws-cdk/core');
 import HelloWorldLambda = require('../lib/hello-world-lambda-stack');
 
-test('Empty Stack', () => {
+test('Stack contains deploy stage', () => {
     const app = new cdk.App();
     // WHEN
     const stack = new HelloWorldLambda.HelloWorldLambdaStack(app, 'MyTestStack');
     // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+    expectCDK(stack).to(
+
+    haveResourceLike("AWS::ApiGateway::Stage", {
+      "StageName": "prod"
+    })
+      
+    )
 });
