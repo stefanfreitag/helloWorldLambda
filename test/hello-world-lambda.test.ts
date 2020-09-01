393a5e3c671c24398a91b628d2e51e9a4508849b
Stefan Freitag <stefan.freitag@udo.edu>
2020-09-01 19:06:55 +0200

    fix: replaced import in test

--- test/hello-world-lambda.test.ts
+++ test/hello-world-lambda.test.ts
@@ -1,9 +1,9 @@
 import { expect as expectCDK, matchTemplate, MatchStyle, haveResourceLike } from '@aws-cdk/assert';
-import cdk = require('@aws-cdk/core');
 import HelloWorldLambda = require('../lib/hello-world-lambda-stack');
+import { App } from '@aws-cdk/core';
 
 test('Stack contains deploy stage', () => {
-    const app = new cdk.App();
+    const app = new App();
     // WHEN
     const stack = new HelloWorldLambda.HelloWorldLambdaStack(app, 'MyTestStack');
     // THEN
