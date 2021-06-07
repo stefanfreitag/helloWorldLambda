b07848bb79e7e0ef780205fd9558a03dd2789f1e
Stefan Freitag <stefan.freitag@udo.edu>
2021-06-07 21:42:01 +0200

    chore: upgrade to cdk v1.107.0

--- .projenrc.js
+++ .projenrc.js
@@ -1,27 +1,27 @@
-const { AwsCdkTypeScriptApp, DependabotScheduleInterval, Stability } = require("projen");
+const { AwsCdkTypeScriptApp, DependabotScheduleInterval, Stability } = require('projen');
 
 const project = new AwsCdkTypeScriptApp({
-    authorName: "Stefan Freitag",
-    authorEmail: "stefan@stefreitag.de",
-    cdkVersion: "1.101.0",
-    name: "hello-world-lambda",
-    description: "A simple Hello World application using an AWS API Gateway and Lambda function.",
-    repository: "https://github.com/stefanfreitag/helloWorldLambda.git",
-    keywords: ["aws", "cdk", "lambda", "apigateway"],
-    dependabotOptions: {
-        scheduleInterval: "monthly",
-    },
-    defaultReleaseBranch: "master",
-    stability: Stability.STABLE,
-    cdkVersionPinning: true,
-    copyrightPeriod: 2020,
-    copyrightOwner: "Stefan Freitag",
-    cdkDependencies: [
-        "@aws-cdk/aws-apigateway",
-        "@aws-cdk/aws-ec2",
-        "@aws-cdk/aws-lambda",
-        "@aws-cdk/core",
-    ],
+  authorName: 'Stefan Freitag',
+  authorEmail: 'stefan@stefreitag.de',
+  cdkVersion: '1.107.0',
+  name: 'hello-world-lambda',
+  description: 'A simple Hello World application using an AWS API Gateway and Lambda function.',
+  repository: 'https://github.com/stefanfreitag/helloWorldLambda.git',
+  keywords: ['aws', 'cdk', 'lambda', 'apigateway'],
+  dependabotOptions: {
+    scheduleInterval: 'monthly',
+  },
+  defaultReleaseBranch: 'master',
+  stability: Stability.STABLE,
+  cdkVersionPinning: true,
+  copyrightPeriod: 2020,
+  copyrightOwner: 'Stefan Freitag',
+  cdkDependencies: [
+    '@aws-cdk/aws-apigateway',
+    '@aws-cdk/aws-ec2',
+    '@aws-cdk/aws-lambda',
+    '@aws-cdk/core',
+  ],
 });
 
 
